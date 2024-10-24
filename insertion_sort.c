077b821251cc4614b22d2243c3e80ad99d7ced29
Andy1599 <andreasbysted@gmail.com>
2024-10-24 15:28:19 +0200

    Opgave 1
    Co-authored-by: AugustOstereje <AugustOstereje@users.noreply.github.com>

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,6 +1,41 @@
 #include "insertion_sort.h"
+#include <stdio.h>
+
 
 node* isort(node *list) {
+    node* sorted = NULL;
+    
+
+    node* next_element = list;
+
+    while(next_element != NULL) {
+        node* current = next_element;
+        next_element = next_element->next;
+        if(sorted = NULL || current->data < sorted->data){
+            current->next = sorted;
+            sorted = current;
+            continue;
+        }
+        node* next_sorted = sorted;
+    while(next_sorted->next != NULL) {
+        if(current->data < next_sorted->next->data){
+            current->next = next_sorted->next;
+            next_sorted->next = current; 
+            break;
+        }
+        next_sorted = next_sorted->next;
+    }
+    if(next_sorted->next == NULL) {
+        next_sorted->next = current;
+        current->next = NULL;
+    }
+
+        
+    }
+
+
+
+
     return list;
 }
 
--- insertion_sort.h
+++ insertion_sort.h
@@ -2,4 +2,11 @@
 
 #include "node.h"
 
+
 node* isort(node* list);
+
+typedef struct node {  
+int data;
+struct node *next;
+} node;
+
