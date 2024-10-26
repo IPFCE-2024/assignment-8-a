baaf246c0b1c9d43ebe60db2b584163d5514276f
AugustOstereje <202408669@post.au.dk>
2024-10-26 12:08:28 +0200

    Update queuetest.c

--- queuetest.c
+++ queuetest.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
 #include "queue.h"
 
 int main() {
+    queue* q;
+    int x0 = 1;
+    int x1 = 2;
+    enqueue(q,1);
+    enqueue(q,2);
+
+    int y0 = dequeue(q);
+    int y1 = dequeue(q);
+
 
 }
